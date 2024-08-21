6841f83884abb6a695f5993686a35c5ad21f0868
Bishoy-at-pieces <bishoy@pieces.app>
2024-08-22 02:24:16 +0300

    add friendly error message

--- rplugin/python3/pieces_python/auth.py
+++ rplugin/python3/pieces_python/auth.py
@@ -1,7 +1,7 @@
-import threading
 from pieces_os_client import OSApi,AllocationsApi, UserProfile
 from typing import Optional
 from .settings import Settings
+import concurrent.futures
 
 class Auth:
 	user_profile:Optional[UserProfile] = None
@@ -11,28 +11,34 @@ class Auth:
 		cls.user_profile = user
 
 	def login(self):
-		thread = OSApi(Settings.api_client).sign_into_os(async_req=True)
-		
-		def run_allocation_async():
-			try:
-				thread.get(120)
-				self.connect
-			except:
-				pass
-		
-		t = threading.Thread(target=run_allocation_async)
-		t.start()
-
-
+		t = OSApi(Settings.api_client).sign_into_os(async_req=True)
+		self.print_info(t,"You have been logged in successfully.",
+			"Oops! Something went wrong. We could not log you in please try again",self.connect)
 
 	def logout(self):
-		OSApi(Settings.api_client).sign_out_of_os(async_req=True)
+		t = OSApi(Settings.api_client).sign_out_of_os(async_req=True)
+		self.print_info(t, "You have been logged out successfully.",
+		 "Oops! Something went wrong. We could not log you out please try again")
 
 	def connect(self):
 		user = self.user_profile
 		if user: # User logged in
-			AllocationsApi(Settings.api_client).allocations_connect_new_cloud(user,async_req=True)
+			t = AllocationsApi(Settings.api_client).allocations_connect_new_cloud(user,async_req=True)
+			self.print_info(t,"Connected to the personal cloud successfully",
+				"Failed to connect from the personal cloud")
 
 	def disconnect(self):
 		if self.user_profile and self.user_profile.allocation: # Check if there is an allocation iterable
-				AllocationsApi(Settings.api_client).allocations_disconnect_cloud(self.user_profile.allocation,async_req=True)
+			t = AllocationsApi(Settings.api_client).allocations_disconnect_cloud(self.user_profile.allocation,async_req=True)
+			self.print_info(t,"Disconnected from the personal cloud successfully","Failed to disconnect from the personal cloud")
+	
+	@staticmethod
+	def print_info(thread,failed_message,success_message,on_success=lambda: None):
+		with concurrent.futures.ThreadPoolExecutor() as executor:
+			future = executor.submit(thread.get, 120)
+			try:
+				future.result()
+				Settings.nvim.out_write(f"{success_message}\n")
+				on_success()
+			except:
+				Settings.nvim.err_write(f"{failed_message}\n")
