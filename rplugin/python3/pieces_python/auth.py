aae3908254782fd8c6c40756589dce090c4ee5c7
Bishoy-at-pieces <bishoy@pieces.app>
2024-08-24 01:30:03 +0300

    fix #41

--- rplugin/python3/pieces_python/auth.py
+++ rplugin/python3/pieces_python/auth.py
@@ -3,6 +3,7 @@ from typing import Optional
 from .settings import Settings
 import concurrent.futures
 from .utils import convert_to_lua_table
+from ._pieces_lib.pieces_os_client.wrapper.websockets import AuthWS
 
 class Auth:
 	user_profile:Optional[UserProfile] = None
@@ -52,13 +53,14 @@ class Auth:
 
 	def connect(self):
 		user = self.user_profile
+		if not user:
+			return
 		compact = self.get_compact_dict(user)
 		compact["is_connecting"] = True
 		self.send_lua(compact)
-		if user: # User logged in
-			t = AllocationsApi(Settings.api_client).allocations_connect_new_cloud(user,async_req=True)
-			self.print_info(t,"Connected to the personal cloud successfully",
-				"Failed to connect from the personal cloud")
+		t = AllocationsApi(Settings.api_client).allocations_connect_new_cloud(user,async_req=True)
+		self.print_info(t,"Connected to the personal cloud successfully",
+			"Failed to connect from the personal cloud")
 
 	def disconnect(self):
 		if self.user_profile and self.user_profile.allocation: # Check if there is an allocation iterable
@@ -75,3 +77,6 @@ class Auth:
 				on_success()
 			except:
 				Settings.nvim.err_write(f"{failed_message}\n")
+
+
+AuthWS(Auth.on_user_callback)
