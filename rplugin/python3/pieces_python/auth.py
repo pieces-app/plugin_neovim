a023863a94de61733544a2497494c48d33d8376e
Bishoy-at-pieces <bishoy@pieces.app>
2024-08-24 10:51:44 +0300

    change api to the wrapper

--- rplugin/python3/pieces_python/auth.py
+++ rplugin/python3/pieces_python/auth.py
@@ -1,9 +1,8 @@
-from ._pieces_lib.pieces_os_client import OSApi,AllocationsApi, UserProfile
+from ._pieces_lib.pieces_os_client import UserProfile
 from typing import Optional
 from .settings import Settings
 import concurrent.futures
 from .utils import convert_to_lua_table
-from ._pieces_lib.pieces_os_client.wrapper.websockets import AuthWS
 
 class Auth:
 	user_profile:Optional[UserProfile] = None
@@ -46,12 +45,12 @@ class Auth:
 			Settings.nvim.command("PiecesAccount")
 			self.connect()
 		callback = on_login_success if show_ui else lambda:None
-		t = OSApi(Settings.api_client).sign_into_os(async_req=True)
+		t = Settings.api_client.os_api.sign_into_os(async_req=True)
 		self.print_info(t,"You have been logged in successfully.",
 			"Oops! Something went wrong. We could not log you in please try again",callback)
 
 	def logout(self):
-		t = OSApi(Settings.api_client).sign_out_of_os(async_req=True)
+		t = Settings.api_client.os_api.sign_out_of_os(async_req=True)
 		self.print_info(t, "You have been logged out successfully.",
 		 "Oops! Something went wrong. We could not log you out please try again")
 
@@ -62,13 +61,13 @@ class Auth:
 		compact = self.get_compact_dict(user)
 		compact["is_connecting"] = True
 		self.send_lua(compact)
-		t = AllocationsApi(Settings.api_client).allocations_connect_new_cloud(user,async_req=True)
+		t = Settings.api_client.allocations_api.allocations_connect_new_cloud(user,async_req=True)
 		self.print_info(t,"Connected to the personal cloud successfully",
 			"Failed to connect from the personal cloud")
 
 	def disconnect(self):
 		if self.user_profile and self.user_profile.allocation: # Check if there is an allocation iterable
-			t = AllocationsApi(Settings.api_client).allocations_disconnect_cloud(self.user_profile.allocation,async_req=True)
+			t = Settings.api_client.allocations_api.allocations_disconnect_cloud(self.user_profile.allocation,async_req=True)
 			self.print_info(t,"Disconnected from the personal cloud successfully","Failed to disconnect from the personal cloud")
 	
 	@staticmethod
