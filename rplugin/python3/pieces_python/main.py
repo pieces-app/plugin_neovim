9b1acac6d33d84df92e2c6caf3f9fecce68a5ffa
Bishoy Hany <167128903+Bishoy-at-pieces@users.noreply.github.com>
2024-08-26 22:06:10 +0300

    error handling

--- rplugin/python3/pieces_python/main.py
+++ rplugin/python3/pieces_python/main.py
@@ -1,19 +1,19 @@
 import pynvim
-
 from .settings import Settings
 from .api import get_version,version_check,is_pieces_opened
-from ._pieces_lib.pieces_os_client.wrapper.basic_identifier import BasicAsset,BasicChat
-from ._pieces_lib.pieces_os_client.wrapper.websockets import *
-from ._pieces_lib.pieces_os_client import FragmentMetadata
-
+from .websockets import ask_stream_ws,base_websocket
+from ._pieces_lib.pieces_os_client import (QGPTStreamInput,
+											QGPTQuestionInput,
+											RelevantQGPTSeeds,
+											ConversationMessageApi,
+											ConversationsApi,
+											FragmentMetadata)
+from .streamed_identifiers.assets_snapshot import AssetSnapshot
+from .websockets.health_ws import HealthWS
 from ._version import __version__
-from .auth import Auth
 from .file_map import file_map
-from .startup import Startup
-from .utils import on_copilot_message
 
 file_map_reverse = {v:k for k,v in file_map.items()}
-
 
 @pynvim.plugin
 class Pieces:
@@ -22,34 +22,61 @@ class Pieces:
 		self.nvim = nvim
 		Settings.nvim = nvim
 		Settings.load_config()
-		self.api_client = Settings.api_client
-		self.auth = Auth()
+		self.api_client = Settings.api_client # Load any host stuff 
 
 	@pynvim.function("PiecesStartup")
 	def startup(self,args):
-		Startup.startup()
+		"""START THE WEBSOCKETS!"""
+		self.health_ws = HealthWS(self._startup)
+		self.health_ws.start()
+
+	def _startup(self):
+		check,plugin = version_check()
+		if check:
+			Settings.get_application() # Connect to the connector API
+			base_websocket.BaseWebsocket.start_all()
+		else:
+			Settings.is_loaded = False
+			self.health_ws.close()
+			self.nvim.async_call(self.nvim.err_write,f"Please update {plugin}\n")
+
 	
-	@pynvim.function('PiecesCopilotSendQuestion')
+	@pynvim.function('PiecesCopilotSendQuestion',sync=True)
 	def send_question(self,args):
-		if args[0].strip():
-			Settings.copilot.stream_question(args[0],on_copilot_message)
+		query = args[0]
+		ask_stream_ws.send_message(QGPTStreamInput(
+			question=QGPTQuestionInput(
+				relevant=RelevantQGPTSeeds(iterable=[]),
+				query=query,
+				application=Settings.get_application().id,
+				model = Settings.model_id
+			),
+			conversation = ask_stream_ws.conversation_id,
+		))
 
 	@pynvim.function('PiecesEditAsset')
 	def edit_asset(self,args):
 		asset_id,data = args
-		BasicAsset(asset_id).raw_content = data
+		AssetSnapshot(asset_id).edit_asset_original_format(data)
 
 	@pynvim.function('PiecesDeleteAsset')
 	def delete_asset(self,args):
 		asset_id = args[0]
-		BasicAsset(asset_id).delete()
+		AssetSnapshot(asset_id).delete()
 
 	@pynvim.function('PiecesGetMessage',sync=True)
 	def get_message(self,args):
 		message_id = args[0]
-		message = Settings.api_client.conversation_message_api.message_specific_message_snapshot(message=message_id,transferables=True)
+<<<<<<< Updated upstream
+		message = ConversationMessageApi(Settings.api_client).message_specific_message_snapshot(message=message_id,transferables=True)
 		return f"{{role = '{message.role.value}', raw = [=[{message.fragment.string.raw}]=]}}"
 
+=======
+		try:
+			message = Settings.api_client.conversation_message_api.message_specific_message_snapshot(message=message_id,transferables=True)
+			return f"{{role = '{message.role.value}', raw = [=[{message.fragment.string.raw}]=]}}"
+		except: pass
+>>>>>>> Stashed changes
 	@pynvim.function("PiecesGetModel",sync=True)
 	def get_model(self,args):
 		return Settings.model_name
@@ -59,45 +86,41 @@ class Pieces:
 	def create_asset(self,args): 
 		try: metadata = FragmentMetadata(ext=file_map_reverse.get(self.nvim.api.buf_get_option(0, 'filetype')))
 		except: metadata = None
-		BasicAsset.create(args[0], metadata)
+		AssetSnapshot.create(args[0], metadata)
 		self.nvim.out_write("Snippet created successfully\n")
 
 	@pynvim.function("PiecesGetModels",sync=True)
 	def get_models(self,args):
-		return"{" + ", ".join(f'"{value}"' for value in Settings.models.keys()) + "}"
+		return"{" + ", ".join(f'"{value}"' for value in Settings.get_models_ids().keys()) + "}"
 
 	@pynvim.function("PiecesChangeModel",sync=True)
 	def change_model(self,args):
 		model_name = args[0]
-		if model_name in self.api_client.available_models_names:
-			Settings.api_client.model_name = model_name
+		if model_name in Settings.get_models_ids().keys():
+			Settings.model_name = model_name
 			return f"Set the current LLM model to {model_name} successfully"
 		return "Invalid Model name"
 
 	@pynvim.function("PiecesSetConversation")
 	def set_conversation(self,args):
+<<<<<<< Updated upstream
+		ask_stream_ws.conversation_id = args[0]
+=======
 		if args:
-			conversation = BasicChat(args[0])
+			try: conversation = BasicChat(args[0])
+			except: conversation = None
 		else:
 			conversation = None
 		Settings.copilot.chat = conversation
+>>>>>>> Stashed changes
 
 	@pynvim.function("PiecesDeleteConversation")
 	def delete_conversation(self,args):
-		try:
-			BasicChat(args[0]).delete()
-		except:
-			pass
+		ConversationsApi(self.api_client).conversations_delete_specific_conversation(args[0])
 
 	@pynvim.function("PiecesVersionCheck", sync=True)
 	def version_check(self,args):
 		return version_check()[0]
-
-	@pynvim.function("PiecesLogin", sync=True)
-	def login_function(self,args):
-		"""first args if true it will show the ui"""
-		if args:
-			self.auth.login(args[0])
 
 	## PYTHON COMMANDS
 	@pynvim.command('PiecesHealth')
@@ -116,53 +139,20 @@ class Pieces:
 	def get_plugin_version(self):
 		self.nvim.out_write(f"{__version__}\n")
 
-	@pynvim.command('PiecesLogin')
-	@is_pieces_opened
-	def login(self):
-		self.auth.login()
-
-	@pynvim.command('PiecesLogout')
-	@is_pieces_opened
-	def logout(self):
-		self.auth.logout()
-
-	@pynvim.command('PiecesConnectCloud')
-	@is_pieces_opened
-	def connect(self):
-		self.auth.connect()
-
-	@pynvim.command('PiecesDisconnectCloud')
-	@is_pieces_opened
-	def disconnect(self):
-		self.auth.disconnect()
-
 	## LUA COMMANDS
 	@pynvim.command("PiecesSnippets")
 	@is_pieces_opened
 	def open_snippets(self):
-		self.nvim.exec_lua("require('pieces.assets').setup()")
+		self.nvim.exec_lua("require('pieces_assets').setup()")
 
 
 	@pynvim.command("PiecesCopilot")
 	@is_pieces_opened
 	def open_copilot(self):
-		self.nvim.exec_lua("require('pieces.copilot').setup()")
+		self.nvim.exec_lua("require('pieces_copilot').setup()")
 
 	@pynvim.command("PiecesConversations")
 	@is_pieces_opened
 	def open_conversations(self):
-		self.nvim.exec_lua("require('pieces.copilot.conversations_ui').setup()")
-
-
-	@pynvim.command("PiecesAccount")
-	@is_pieces_opened
-	def auth_command(self):
-		self.nvim.exec_lua("require('pieces.auth').setup()")
-
-	@pynvim.command('PiecesCreateSnippet', range='', nargs='*')
-	@is_pieces_opened
-	def pieces_create_snippet(self, args, range):
-		line1 = range[0]
-		line2 = range[1]
-		self.nvim.exec_lua(f"require('pieces.assets.create').setup({line1}, {line2})")
+		self.nvim.exec_lua("require('pieces_copilot.conversations_ui').setup()")
 
