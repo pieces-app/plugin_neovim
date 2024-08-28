8dbc6fb5c593cd1dd1a7bc93542fa7c4807ff525
Bishoy-at-pieces <bishoy@pieces.app>
2024-08-28 04:07:54 +0300

    refactor conversation and assets ui

--- lua/pieces/copilot/conversations_ui.lua
+++ lua/pieces/copilot/conversations_ui.lua
@@ -1,51 +1,54 @@
 local Popup = require('nui.popup')
 local conversations_module = require('pieces.copilot.conversations')
 local copilot_module = require('pieces.copilot')
+local ListUpdater = require("pieces.list_updater")
+local make_buffer_read_only = require("pieces.utils").make_buffer_read_only
 local M = {}
-local current_index = 1
 local conversations = conversations_module.conversations
 local results_popup
 
-local function update_list()
-	if results_popup and type(results_popup.bufnr) == "number" and not vim.api.nvim_buf_is_valid(results_popup.bufnr) then
-		return
-	end
+local updater = ListUpdater:new(results_popup,1,conversations,
+	function(conversation)
+        return conversation.name:gsub("\n", "\\n")
+    end,
+    function(conversation)
+        return conversation.annotation
+	end,
+	function (item)
+	    results_popup:unmount()
+	    copilot_module.setup() -- reset the copilot view
+	    for k, v in pairs(item.messages) do
+	        if v == -1 then -- message is deleted
+	            goto continue
+	        end
 
-    local lines = {}
-    local end_col, start_col, annotation_index
+	        local table_str = vim.fn.PiecesGetMessage(k)
+	        if table_str == nil then
+	            vim.notify("Not a valid conversation", vim.log.levels.ERROR)
+	            break
+	        end
+	        local func, err = load("return " .. table_str)
+	        if not func then
+	            print("Error loading string:", err)
+	            return
+	        end
+	        local message = func()
+	        copilot_module.append_to_chat(message.raw, message.role)
 
-    for i, conversation in ipairs(conversations) do
-        local base_name = conversation.name:gsub("\n", "\\n")
-        if i == current_index then
-            local highlighted_line = "> " .. base_name .. " "
-            start_col = #highlighted_line
-            highlighted_line = highlighted_line .. conversation.annotation
-            end_col = start_col + #conversation.annotation
-            annotation_index = i - 1 -- Convert to 0-based index for highlighting
-            table.insert(lines, highlighted_line)
-        else
-            table.insert(lines, " " .. base_name)
-        end
-    end
-    local success, err = pcall(function()
-	    vim.api.nvim_buf_set_lines(results_popup.bufnr, 0, -1, false, lines)
-
-	    -- Apply the highlight if annotation_index is valid
-	    if annotation_index then
-	        vim.api.nvim_buf_add_highlight(results_popup.bufnr, -1, "PiecesAnnotation", annotation_index, start_col, end_col)
+	        ::continue::
 	    end
-
-	    local win_height = vim.api.nvim_win_get_height(results_popup.winid) - 5 -- Removing the borders
-	    local cursor_line = current_index - 1 -- Convert to 0-based index for nvim_win_set_cursor
-	    if cursor_line >= win_height or cursor_line - win_height < 0 then
-	        vim.api.nvim_win_set_cursor(results_popup.winid, { current_index, 0 })
+	    vim.fn.PiecesSetConversation(item.id)
+	end,
+	function (item)
+	    local choice = vim.fn.confirm("Are you sure you want to delete '" .. item.name .."'", "&Yes\n&No", 1)
+	    if choice == 1 then
+	        conversations_module.delete(item.id)
 	    end
 	end)
 
-end
 function M.update()
-	if results_popup and type(results_popup) == "number" then
-		update_list()
+	if results_popup and type(results_popup.bufnr) == "number" then
+		updater:update()
 	end
 end
 
@@ -70,78 +73,9 @@ function M.setup()
 			winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
 		},
 	})
-
-
-	local function down_keymap()
-		if current_index < #conversations then
-			current_index = current_index + 1
-			update_list()
-		end
-	end
-
-	local function up_keymap()
-		if current_index > 1 then
-			current_index = current_index - 1
-			update_list()
-		end
-	end
-	local function enter_keymap()
-		results_popup:unmount()
-		copilot_module.setup() -- reset the copilot view
-		for k,v in pairs(conversations[current_index].messages) do
-			if v == -1 then -- message is deleted
-				goto continue
-			end
-
-			local table_str = vim.fn.PiecesGetMessage(k)
-			if table_str == nil then
-				vim.notify("Not a valid conversation", vim.log.levels.ERROR)
-				break
-			end
-			local func, err = load("return " .. table_str)
-			if not func then
-			    print("Error loading string:", err)
-			    return
-			end
-			local message = func()
-			copilot_module.append_to_chat(message.raw,message.role)
-
-			::continue::
-		end
-		vim.fn.PiecesSetConversation(conversations[current_index].id)
-	end
-	local function delete_keymap()
-		local choice = vim.fn.confirm("Are you sure you want to delete '" .. conversations[current_index].name .."'", "&Yes\n&No", 1)
-	    if choice == 1 then
-			conversations_module.delete(conversations[current_index].id)
-		end
-	end
-	-- Key mappings for navigation
-	local keymaps = {
-		["<Up>"] = up_keymap,
-		["<Down>"] = down_keymap,
-		["<esc>"] = function() results_popup:unmount() end,
-		["<enter>"] = enter_keymap,
-		["<Del>"] = delete_keymap,
-		["<kDel>"] = delete_keymap,
-		["<BS>"] = delete_keymap
-	}
-	local modes = { "i", "n" }
-
-	for key, func in pairs(keymaps) do
-		for _, mode in ipairs(modes) do
-			results_popup:map(mode, key, func, { noremap = true })
-		end
-	end
-
-
-
-	-- Mount the layout
-	results_popup:mount()
-	update_list()
-
-	vim.api.nvim_set_current_win(results_popup.winid)
-
+	make_buffer_read_only(results_popup.bufnr)
+	updater.results_popup = results_popup -- Update the new result popup
+	updater:mount()
 end
 
 return M
---
+++ lua/pieces/list_updater.lua
@@ -0,0 +1,118 @@
+-- Define the ListUpdater class
+local ListUpdater = {}
+ListUpdater.__index = ListUpdater
+
+-- Constructor for ListUpdater
+function ListUpdater:new(results_popup,
+    current_index,
+    items,
+    get_base_name,
+    get_annotation,
+    enter_keymap,
+    delete_keymap,
+    on_update)
+    local instance = {
+        results_popup=results_popup,
+        current_index = current_index,
+        items = items,
+        get_base_name=get_base_name,
+        get_annotation=get_annotation,
+        enter_keymap=enter_keymap,
+        delete_keymap=delete_keymap,
+        on_update=on_update
+    }
+    setmetatable(instance, ListUpdater)
+    return instance
+end
+
+
+function ListUpdater:_update_list_common()
+    if self.results_popup == nil or type(self.results_popup.bufnr) ~= "number" or not vim.api.nvim_buf_is_valid(self.results_popup.bufnr) then
+        return
+    end
+
+    local lines = {}
+    local end_col, start_col, annotation_index
+
+    for i, item in ipairs(self.items) do
+        local base_name = self.get_base_name(item)
+        if i == self.current_index then
+            -- Highlighted line (current selection)
+            local highlighted_line = "> " .. base_name .. " "
+            local annotation = self.get_annotation(item):gsub("\n", " ")
+            start_col = #highlighted_line
+            highlighted_line = highlighted_line .. annotation
+            end_col = start_col + #annotation
+            annotation_index = i - 1 -- Convert to 0-based index for highlighting
+            table.insert(lines, highlighted_line)
+        else
+            table.insert(lines, " " .. base_name)
+        end
+    end
+
+    vim.api.nvim_buf_set_lines(self.results_popup.bufnr, 0, -1, false, lines)
+
+    -- Apply the highlight if annotation_index is valid
+    if annotation_index then
+        vim.api.nvim_buf_add_highlight(self.results_popup.bufnr, -1, "PiecesAnnotation", annotation_index, start_col, end_col)
+    end
+
+    local win_height = vim.api.nvim_win_get_height(self.results_popup.winid) - 5 -- Removing the borders
+    local cursor_line = self.current_index - 1 -- Convert to 0-based index for nvim_win_set_cursor
+    if cursor_line >= win_height or cursor_line - win_height < 0 then
+        vim.api.nvim_win_set_cursor(self.results_popup.winid, { self.current_index, 0 })
+    end
+end
+
+function ListUpdater:update()
+    self:_update_list_common()
+    if type(self.on_update) == "function" then
+        self.on_update(self)
+    end
+end
+
+function ListUpdater:down_keymap()
+    if self.current_index < #self.items then
+        self.current_index = self.current_index + 1
+        self:update()
+    end
+end
+
+function ListUpdater:up_keymap()
+    if self.current_index > 1 then
+        self.current_index = self.current_index - 1
+        self:update()
+    end
+end
+
+
+function ListUpdater:setup_keymaps()
+    local keymaps = {
+        ["<Up>"] = function() self:up_keymap() end,
+        ["<Down>"] = function() self:down_keymap() end,
+        ["<esc>"] = function() self.results_popup:unmount() end,
+        ["<enter>"] = function() self.enter_keymap(self.items[self.current_index]) end,
+        ["<Del>"] = function() self.delete_keymap(self.items[self.current_index]) end,
+        ["<kDel>"] = function() self.delete_keymap(self.items[self.current_index]) end,
+        ["<BS>"] = function() self.delete_keymap(self.items[self.current_index]) end
+    }
+    local modes = { "i", "n" }
+
+    for key, func in pairs(keymaps) do
+        for _, mode in ipairs(modes) do
+            self.results_popup:map(mode, key, func, { noremap = true })
+        end
+    end
+end
+function ListUpdater:setup()
+    self:setup_keymaps()
+    self:update()
+end
+function ListUpdater:mount()
+    self.results_popup:mount()
+    vim.api.nvim_set_current_win(self.results_popup.winid)
+    self:setup()
+end
+
+return ListUpdater
+
