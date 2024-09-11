d6f8b2c9cee80df1362bfa93d30e1cc5ee70973e
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-12 01:33:08 +0300

    fix create snippet

--- lua/pieces/assets/create.lua
+++ lua/pieces/assets/create.lua
@@ -1,11 +1,12 @@
 local M = {}
-function M.setup(l1,l2)
+function M.setup()
     local start_pos = vim.fn.getpos("'<")
     local end_pos = vim.fn.getpos("'>")
     local lines = vim.fn.getline(start_pos[2], end_pos[2])
 
     if #lines == 0 then
-        return ""
+        local buf = vim.api.nvim_get_current_buf()
+        lines = vim.api.nvim_buf_get_lines(buf, 0,-1 , false)
     end
 
     lines[#lines] = string.sub(lines[#lines], 1, end_pos[3])
