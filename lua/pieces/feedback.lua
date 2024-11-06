096d08d6b58dfddead4fe508951f48fde81be9df
Bishoy Hany <167128903+Bishoy-at-pieces@users.noreply.github.com>
2024-11-06 22:19:03 +0200

    fix wording in the contribute command

--- lua/pieces/feedback.lua
+++ lua/pieces/feedback.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_user_command('PiecesFeedback', function()
 We always care about your feedback.
 Feel free to share your experience with us.
 https://github.com/pieces-app/plugin_neovim/discussions/194
-Would you like to open the feedback page in your browser? ]=],
+Would you like to open GitHub? ]=],
         "&Yes\n&No", 1)
     if choice == 1 then
         vim.fn.PiecesOpenLink("https://github.com/pieces-app/plugin_neovim/discussions/66")
