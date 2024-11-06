5d8b87ee7afa259fea1d184f715032254f47e93b
Bishoy Hany <167128903+Bishoy-at-pieces@users.noreply.github.com>
2024-11-06 17:33:28 +0200

    fix close the split view if the copilot is closed

--- lua/pieces/utils.lua
+++ lua/pieces/utils.lua
@@ -3,12 +3,11 @@ local function make_buffer_read_only( bufnr )
   local group = vim.api.nvim_create_augroup("PreventInsertMode", { clear = true })
 
   -- Add autocommands to the group for the new buffer
-  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertCharPre" }, {
+  vim.api.nvim_create_autocmd({ "InsertEnter",  "BufEnter" }, {
     group = group,
     buffer = bufnr,
     callback = function()
       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
-      vim.notify("Insert mode is disabled in this popup", vim.log.levels.WARN)
     end})
 end
 
